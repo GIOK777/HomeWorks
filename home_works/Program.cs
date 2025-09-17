4a19a2ec7b68a3ae38186f079c52ab93f3b91f3b
Giorgi Kitiashvili <gioqitiashvili@yahoo.com>
2025-09-17 21:27:08 +0400

    first comit

---
+++ home_works/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace home_works
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
